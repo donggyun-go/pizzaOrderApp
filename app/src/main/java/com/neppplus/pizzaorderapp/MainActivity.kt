80d729b844e375d675e47ad09193d2619cde2aa9
donggyun-go <ehdrbsr43@naver.com>
2024-05-08 12:48:52 +0900

    BaseActivity Commit

--- app/src/main/java/com/neppplus/pizzaorderapp/MainActivity.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp/MainActivity.kt
@@ -2,19 +2,26 @@ package com.neppplus.pizzaorderapp
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : BaseActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setupEvents()
+        setValues()
     }
+
+    //버튼 클릭등의 이벤트 관련 코드 모음
+    override fun setupEvents() {
+
+    }
+
+    // 화면의 데이터를 표시하기 위한 코드 모음
+    override fun setValues() {
+
+    }
+
 }
